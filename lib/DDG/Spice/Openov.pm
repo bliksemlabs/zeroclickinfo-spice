2961545d2f7e7e9f557d9e8f7bdbf21297545dea
Stefan de Konink <stefan@konink.de>
2016-01-05 22:49:25 +0100

    Push to test the incorrect departure url

---
+++ lib/DDG/Spice/Openov.pm
@@ -0,0 +1,36 @@
+package DDG::Spice::Openov;
+
+# ABSTRACT: Write an abstract here
+# Start at https://duck.co/duckduckhack/spice_overview if you are new
+# to instant answer development
+
+use DDG::Spice;
+
+# Caching - https://duck.co/duckduckhack/spice_advanced_backend#caching-api-responses
+spice is_cached => 1;
+spice proxy_cache_valid => "200 1d"; # defaults to this automatically
+
+spice wrap_jsonp_callback => 1; # only enable for non-JSONP APIs (i.e. no &callback= parameter)
+
+# API endpoint - https://duck.co/duckduckhack/spice_attributes#spice-codetocode
+spice to => 'http://ovapi.nl/v2/search?type[]=stop_area&q=$1';
+spice alt_to => {
+    departures => {
+        is_cached => 1,
+        proxy_cache_valid => '200 1m',
+        to => 'http://ovapi.nl/v2/stop_area/$1/departures',
+        wrap_jsonp_callback => 1
+    }
+};
+
+# Triggers - https://duck.co/duckduckhack/spice_triggers
+triggers any => 'vertrektijden';
+
+# Handle statement
+handle remainder => sub {
+
+    # Query is in $_...if you need to do something with it before returning
+    return $_;
+};
+
+1;
---
+++ share/spice/openov/openov.js
@@ -0,0 +1,50 @@
+(function (env) {
+    "use strict";
+    env.ddg_spice_openov = function(api_result){
+        if (api_result.search.length == 0) {
+            return Spice.failed('openov');
+        }
+
+        var stop_area_parts = api_result.search[0]._links[0].href.split('/');
+        var stop_area_code = stop_area_parts[stop_area_parts.length - 1];
+        $.getScript("/js/spice/openov/departures/" + stop_area_code);
+    };
+
+    env.ddg_spice_openov_departures = function(api_result){
+
+        // Validate the response (customize for your Spice)
+        if (!api_result || api_result.error) {
+            return Spice.failed('openov');
+        }
+
+        // Render the response
+        Spice.add({
+            id: "openov",
+
+            // Customize these properties
+            name: "Vertrektijden",
+            data: api_result.search,
+            meta: {
+                sourceName: "Example.com",
+                sourceUrl: 'http://example.com/url/to/details/' + api_result.name
+            },
+            normalize: function(item) {
+                return {
+                    // customize as needed for your chosen template
+                    title: item.name,
+                    subtitle: item.type
+                };
+            },
+            templates: {
+                group: 'base',
+                detail: false,
+                item_detail: false,
+                options: {
+                    content: Spice.openov.vertrektijd_item,
+                    moreAt: true
+                }
+            }
+        });
+    };
+
+}(this));
